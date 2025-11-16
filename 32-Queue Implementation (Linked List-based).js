50311d19c4879430df3f8db23b7e07f8d805cf8a
Jatin Dhamija <jd.codebase@gmail.com>
2025-11-16 12:57:37 +0530

    32-Queue Implementation (Linked List-based)

---
+++ 32-Queue Implementation (Linked List-based).js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
+}
